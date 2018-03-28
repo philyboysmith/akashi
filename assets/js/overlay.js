8aec23082fa0a8a1a3350fa6f854fc61e2a8f002
Phil Smith <phil@amillionmonkeys.co.uk>
2018-03-28 15:35:21 +0100

    fix z-index

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -9,6 +9,7 @@ window.onload = function() {
         iframe.style.left = "0";
         iframe.style.width = "100%";
         iframe.style.height = "100%";
+        iframe.style.zIndex = "999999999";
         iframe.src = "https://philyboysmith.github.io/akashi/overlay.html";
         document.body.appendChild(iframe);
     }
